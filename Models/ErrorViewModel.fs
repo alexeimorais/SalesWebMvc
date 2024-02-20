5cf2c61bb3198abb53354f9dd4b8e67dd8bde3b6
Alexei Morais <alexei.morais@gmail.com>
2024-02-19 21:15:00 -0300

    First Modelo-Controller-View - Department

--- Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace SalesWebMvc.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
