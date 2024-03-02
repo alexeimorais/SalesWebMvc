42040539dd6d2447a305ea2da6dc7bd92eb3b4e9
Alexei Morais <alexei.morais@gmail.com>
2024-03-01 22:07:52 -0300

    Other entities and second migration

--- Models/Department.cs
+++ Models/Department.cs
@@ -4,5 +4,25 @@
     {
         public int id { get; set; }
         public string name { get; set; }
+
+        public ICollection<Seller> sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void AddSller(Seller seller) 
+        {
+            sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime Initial, DateTime Final)
+        {
+            return sellers.Sum(s => s.TotalSales(Initial, Final));
+        }
     }
 }
---
+++ Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Bulled = 1,
+        Canceled = 2
+    }
+}
