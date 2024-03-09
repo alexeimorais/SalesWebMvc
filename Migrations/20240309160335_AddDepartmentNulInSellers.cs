193f390c4d9cd9268c5d733c7f23e213743089a7
Alexei Morais <alexei.morais@gmail.com>
2024-03-09 13:05:58 -0300

    Simple create form

---
+++ Migrations/20240309160335_AddDepartmentNulInSellers.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentNulInSellers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_Departmentid",
+                table: "Sellers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Departmentid",
+                table: "Sellers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_Departmentid",
+                table: "Sellers",
+                column: "Departmentid",
+                principalTable: "Department",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_Departmentid",
+                table: "Sellers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Departmentid",
+                table: "Sellers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_Departmentid",
+                table: "Sellers",
+                column: "Departmentid",
+                principalTable: "Department",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
